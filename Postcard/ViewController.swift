37caddec42ef7072288ea6254391a05d568d738e
WPP Administrator <wppa@wpp-macbook-pro-m20131126-4-ArunJBernard.local>
2014-10-30 07:26:18 -0700

    Firt commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var receiver: UITextField!
+    @IBOutlet weak var messageText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMessagePressed(sender: UIButton) {
+        
+        messageLabel.text = messageText.text
+        messageText.text = nil
+        messageLabel.hidden = false
+        sender.backgroundColor = UIColor.blueColor()
+        messageText.resignFirstResponder()
+        sender.setTitle("Message Sent", forState: UIControlState.Normal)
+    }
 
 }
 
